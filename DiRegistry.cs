f79a33ef9956435ea6f35935173a1527dd1d2c86
Atul_Sirsode <Atul_Sirsode@dell.com>
2023-05-08 17:48:46 +0530

    adding initial commit

---
+++ DiRegistry.cs
@@ -0,0 +1,50 @@
+﻿using System.Reflection;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DIServiceContainer
+{
+    public interface IBaseService { }
+    public static class DiRegistry
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services, IEnumerable<Assembly> assemblies, ServiceLifetime lifetime = ServiceLifetime.Singleton)
+        {
+            var enumerable = assemblies as Assembly[] ?? assemblies.ToArray();
+            if (!enumerable.Any())
+            {
+                throw new ArgumentNullException($"no assemblies provided for registration");
+            }
+
+            var interFaceType = typeof(IBaseService);
+            var implementations = enumerable
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(type => type.IsClass && !type.IsAbstract && interFaceType.IsAssignableFrom(type));
+
+            foreach (var implementation in implementations)
+            {
+                foreach (var implementedInterface in implementation.GetInterfaces().Where(i => i != interFaceType))
+                {
+                    switch (lifetime)
+                    {
+                        case ServiceLifetime.Singleton:
+                            services.AddSingleton(implementedInterface, implementation);
+                            break;
+                        case ServiceLifetime.Transient:
+                            services.AddTransient(implementedInterface, implementation);
+                            break;
+                        case ServiceLifetime.Scoped:
+                            services.AddScoped(implementedInterface, implementation);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException(nameof(lifetime), lifetime, null);
+                    }
+
+                }
+            }
+
+            return services;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# DIServiceContainer
+# Overview
+
+**The package is a .NET library written in C# that helps to register services in Dependency Injection (DI).**
+
+
+## Features
+
+- Simplifies the process of registering services in DI container.
+- Makes it easy to manage dependencies across the application.
+- Supports different scopes for registered services.
+- Default Service Lifetime scope is Singleton.
+
+## Installation
+The package can be installed from the NuGet Package Manager or via the command line:
+
+`
+Install-Package DIServiceContainer
+`
+
+## Usage
+To use the package, add the following code to your application:
+using DIRegistration;
+
+Let say you have Services in Your Project 
+for ex : 
+```csharp
+Public Interface IEmailService {void SendEmail();}
+Public Interface IMessageService { String SendMessage();}
+
+Public class EmailService : IEmailService 
+{
+ //TODO :: implementation
+}
+
+Public class MessageService : IMessageService 
+{
+ //TODO :: implementation
+}
+```
+
+Now, instead of registering services individually like 
+
+```csharp
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddSingleTon<IEmailService,EmailService>();
+    builder.Services.AddSingleTon<IMessageService,MessageService>();
+}
+```
+you can register those service with single line
+
+
+```csharp
+var builder = WebApplication.CreateBuilder(args);
+{
+    builder.Services.AddServices(new[] { typeof(Program).Assembly });
+}
+```
+
+# Disclaimer
+
+This is an educational project. The source code is licensed under the MIT license.
+
+# License
+This package is licensed under the MIT License.
